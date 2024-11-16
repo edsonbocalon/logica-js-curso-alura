8c6be81a3f430e088c9ffd45b9d3c3ddd39c07dd
Edson Bocalon <108438046+edsonbocalon@users.noreply.github.com>
2024-11-16 15:56:07 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+alert('Boas vindas ao jogo do número secreto');
+let numeroSecreto = 28;
+let chute = prompt('Escolha um número entre 1 e 50');
+
+if(chute == numeroSecreto){
+    console.log('Isso aí! Você descobriu o número secreto (28)');
+    
+}
